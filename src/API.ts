2aeb0f6d88b6173cb9d2b67954121ae2e518af41
Aysha <42186490+Aysha-Mohamed@users.noreply.github.com>
2023-03-29 12:27:19 +0200

    Update API.ts

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,30 @@
+
+
+//the response is 
+// category: "Entertainment: Video Games"
+// correct_answer: "Lambda"
+// difficulty: "easy"
+// incorrect_answers: (3) ['Omega', 'Alpha', 'Sigma']
+// question: "Which Greek letter represents the &quot;Half-Life&quot; logo?"
+// type: "multiple"
+
+//specify the type for each questions
+
+export type Questions = {
+    category: string,
+    correct_answer: string,
+    difficulty: string,
+    incorrect_answers: string[],
+    question: string,
+    type: string
+}
+
+//we are going to create another type for al questions to show in UI.
+
+export type QuestionState = Questions & {answers: string[]};
+
+
+
 export enum Difficulty{
     EASY = "easy",
     MEDIUM = "medium",
